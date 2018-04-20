ed19545c3bf1c8dc9f6d8d33f143f9fa07a0eec9
asterrypm <asterrypm@gmail.com>
2018-04-20 11:30:17 -0400

    revised server with route requires

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/ Dependencies
+// Dependencies
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
@@ -7,9 +7,21 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
 
+var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+// Routes
+// =============================================================
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
