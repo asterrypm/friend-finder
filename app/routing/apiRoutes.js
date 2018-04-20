5ab7c3ffe95d7d7626a3e7a70ecbef05d80646d5
asterrypm <asterrypm@gmail.com>
2018-04-20 13:39:49 -0400

    updated server.js, html and apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,67 @@
 var friends = require('../data/friends.js')
+module.exports = function(app) {
+
+// Basic route that sends the user first to the AJAX Page
+// =============================================================
+
+  //A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+  app.get('/api/friends', function(req, res) {
+    res.json(friends);
+  });
+
+ //A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+
+  app.post('/api/friends', function(req, res) {
+    // Get the new user data out of the request body
+    var newUser = req.body;
+    // Do some looping and calculations to determine the best match
+    var bestMatch = function(newUser) {
+      // loops and calculations
+      // ???
+      return friends[indexOfBestMatch]
+    };
+    // Add the newUser to the "database"
+    friends.push(newUser);
+    // Send response to client with the best match
+    res.json(bestMatch);
+  });
+};
+
+
+
+
+
+
+
+
+var bestMatch = function (newUser) {
+  // Declare variable that will hold the bestMatch
+  var indexOfBestMatch;
+  // Declare variable that will hold the current lowest difference
+  var diffOfBestMatch = /* a really large placeholder number */;
+  
+  // Nested for loops
+  for (/* loop over the friends array */) {
+
+    // Define variable to store the cumulative difference of newUser and friend
+    var total = 0;
+
+    for (/* loop over scores property of each friend */) {
+
+      // Add absolute value of difference to the total
+      total += Math.abs(/* newUser score MINUS friend's score */);
+
+    }
+
+    if (/* total is lower than diffOfBestMatch */) {
+      /* then the indexOfBestMatch is the index of the current loop */
+      /* also set diffOfBestMatch to hold current loop's 'total' value */
+    } else {
+      /* do nothing, this friend is not the best match */
+    }
+  }
+  return friends[indexOfBestMatch]
+};
+
+
+Add CommentCollapse 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,24 +1,22 @@
 var friends = require('../data/friends.js')
-
+var path = require('path');
+module.exports = function(app) {
 
 // Basic route that sends the user first to the AJAX Page
 // =============================================================
 
  // A GET Route to `/survey` which should display the survey page.
 app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "survey.html"));
+  res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 
 
  //A default, catch-all route that leads to `home.html` which displays the home page. 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
+  res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
-
-
-
-
+}
 
 
 
