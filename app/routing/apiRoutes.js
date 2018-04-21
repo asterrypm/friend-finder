b1c63bcbc535771ad1d79711140a9eb431f12aaa
asterrypm <asterrypm@gmail.com>
2018-04-21 07:11:18 -0400

    revised home and survey html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,8 +16,33 @@ module.exports = function(app) {
     var newUser = req.body;
     // Do some looping and calculations to determine the best match
     var bestMatch = function(newUser) {
-      // loops and calculations
-      // ???
+    	
+	  // Declare variable that will hold the bestMatch
+	  var indexOfBestMatch;
+	  // Declare variable that will hold the current lowest difference
+	  var diffOfBestMatch = /* a really large placeholder number */;
+	  
+	  // Nested for loops
+	  for (/* loop over the friends array */) {
+
+	    // Define variable to store the cumulative difference of newUser and friend
+	    var total = 0;
+
+	    for (/* loop over scores property of each friend */) {
+
+	      // Add absolute value of difference to the total
+	      total += Math.abs(/* newUser score MINUS friend's score */);
+
+	    }
+
+	    if (/* total is lower than diffOfBestMatch */) {
+	      /* then the indexOfBestMatch is the index of the current loop */
+	      /* also set diffOfBestMatch to hold current loop's 'total' value */
+	    } else {
+	      /* do nothing, this friend is not the best match */
+	    }
+	  }
+	  
       return friends[indexOfBestMatch]
     };
     // Add the newUser to the "database"
@@ -34,34 +59,7 @@ module.exports = function(app) {
 
 
 
-var bestMatch = function (newUser) {
-  // Declare variable that will hold the bestMatch
-  var indexOfBestMatch;
-  // Declare variable that will hold the current lowest difference
-  var diffOfBestMatch = /* a really large placeholder number */;
-  
-  // Nested for loops
-  for (/* loop over the friends array */) {
 
-    // Define variable to store the cumulative difference of newUser and friend
-    var total = 0;
-
-    for (/* loop over scores property of each friend */) {
-
-      // Add absolute value of difference to the total
-      total += Math.abs(/* newUser score MINUS friend's score */);
-
-    }
-
-    if (/* total is lower than diffOfBestMatch */) {
-      /* then the indexOfBestMatch is the index of the current loop */
-      /* also set diffOfBestMatch to hold current loop's 'total' value */
-    } else {
-      /* do nothing, this friend is not the best match */
-    }
-  }
-  return friends[indexOfBestMatch]
-};
 
 
 Add CommentCollapse 
